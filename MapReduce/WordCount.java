98e8e7147a785a8e5218abd80c592171714fd218
ywchiu <tr.ywchiu@gmail.com>
2016-03-13 14:41:56 +0800

    add lc

--- MapReduce/WordCount.java
+++ MapReduce/WordCount.java
@@ -56,9 +56,9 @@ public class WordCount {
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
 		FileInputFormat.addInputPath(job, new Path(
-				"hdfs://localhost:8020/user/cloudera/data/wc.txt"));
+				"file:///home/hadoop/wc.txt"));
 		FileOutputFormat.setOutputPath(job, new Path(
-				"hdfs://localhost:8020/user/cloudera/out"));
+				"file:///home/hadoop/out"));
 		//FileInputFormat.addInputPath(job, new Path(args[0]));
 		//FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
